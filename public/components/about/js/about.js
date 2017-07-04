43bf9f44df829cc1a295c8b5a2b395568620fca9
garkalgu <garkalgu@cisco.com>
2017-07-04 10:42:35 +0530

    Infra in place. Start writing code - 1

--- public/components/about/js/about.js
+++ public/components/about/js/about.js
@@ -1,9 +1,19 @@
 resumeApp.controller('aboutController', function aboutController($scope, $http) {
 	$http.get("http://localhost:3000/profile").then(function (response) {
 		var profileResponse = response.data[0];
-		$scope.Name = profileResponse.Name;
+		$scope.Name = profileResponse.name;
+		$scope.aboutMe = profileResponse.aboutMe;
 	});
 	$http.get("http://localhost:3000/hobbies").then(function (response) {
-		$scope.hobbyResponse = response.data;
+		var hobbyResponse = response.data;
+		$scope.hobbyList = "";
+		for (var i = 0; i < hobbyResponse.length; i++) {
+			$scope.hobbyList += hobbyResponse[i].name;
+			$scope.hobbyList += (i != hobbyResponse.length-1)?", ":".";
+		}
+		$scope.hobbyList += "\n";
+		for (var i = 0; i < hobbyResponse.length; i++) {
+			$scope.hobbyList += hobbyResponse[i].description + "\n";
+		}
 	});
 });
